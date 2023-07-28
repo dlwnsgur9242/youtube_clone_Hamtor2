14bdd3e8203c82bca9970087f0bd5fe2c8c6e049
dlwnsgur9242 <dlwnsgur9242@naver.com>
2023-07-28 19:11:17 +0900

    FiX

---
+++ README copy.md
@@ -0,0 +1,48 @@
+# youtube_clone_Hamtor2
+
+### 우리 햄톨2조🐹 다 같이 화이팅 해봐요!🌟
+
+
+# 브랜치 구성 
+### 예시
+- html
+   - html 코드 전체
+
+- css_1
+   - css 전체 버튼 메뉴화면 구성
+
+- css_2
+   - css 폰트, 영상스타일
+
+- js_1
+   - 기본적인 동작
+
+- js_2
+   - AI 
+
+# 커밋 메세지 룰
+    Line 1: 제목
+            - '태그(레이어): 기능' 으로 입력
+            - 예: Feat(H): Sidebar
+
+            # 태그
+            - 명령형 동사 사용. 대문자로 시작
+            - 예: Init (첫 업로드), Feat (새로운 기능), Fix (수정), Remove (삭제)
+
+            # 레이어
+            - H (Home 페이지) / C (Channel 페이지) / V (Video 페이지)
+
+            # 기능
+            - 사용한 변수명 또는 함수명 입력
+
+    Line 2: 공란
+
+    Line 3: 본문
+            - What, Why 등 작성
+
+# camelCase 사용
+
+### 변수 사용 룰 (아래 셋 중 선정 필요)
+    - Button vs Btn (자음만)
+    - Number vs Num (축약형)
+    - 함수는 동사로 시작
---
+++ home.js
@@ -0,0 +1,103 @@
+
+// 비디오 목록 API 불러와 홈 화면에 보여주기
+function loadVideoList() {
+
+    // 서버와 통신하기 위한 XMLHttpRequest 객체 생성
+    let xhr = new XMLHttpRequest();
+    let data;
+
+    xhr.open("GET", "http://oreumi.appspot.com/video/getVideoList");
+    xhr.send();
+
+    xhr.onload = async () => {
+        // 받아 온 JSON 형식의 응답데이터를 비동기적으로 파싱하여 data 변수에 할당
+        data = await (JSON.parse(xhr.responseText));
+        // 받아 온 비디오리스트를 가공하여 홈 화면에 보여줌
+        makeHomeDiv(data);
+    }
+}
+
+// 비디오의 세부정보 API 불러오기
+function loadVideoDetail(data) {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+
+        // ${data.video_id} 를 사용하여 data 객체의 video_id 값을 동적으로 넣어줌 (id # 순서대로 불러오지는 못했음)
+        xhr.open("GET", `http://oreumi.appspot.com/video/getVideoInfo?video_id=${data.video_id}`);
+        xhr.send();
+
+        xhr.onload = () => {
+            const dataDetail = JSON.parse(xhr.responseText);
+            resolve(dataDetail);
+        };
+
+        xhr.onerror = () => {
+            reject(new Error('Failed to load video detail'));
+        };
+    });
+}
+
+// 업로더 프로필 이미지 랜덤으로 띄우기 (랜덤이라기 보단 배열 순서대로 돌아가며 뜸)
+function randomAvatarPic(video_id){
+    let avatarPic = ['AlanCooper.svg', 'AlexisSears.svg', 'AnnaWhite.svg', 'JamesGouse.svg', 'JesicaLambert.svg', 'MainProfile.svg', 'MarcusLevin.svg', 'SkylarDias.svg'];
+
+    let index = video_id % avatarPic.length;
+    return avatarPic[index]
+}
+
+function makeHomeDiv(datas) {
+
+    const videoList = document.getElementById('Purple')
+
+    for (let data of datas) {
+        let dataDetail = loadVideoDetail(data)
+            .then((dataDetail) => {
+
+                let avatarName = randomAvatarPic(dataDetail.video_id);
+
+                const thumbnailItem = document.createElement("div");
+                thumbnailItem.classList.add("thumbnail_item");
+
+                const thumbnailImages = document.createElement("img");
+                thumbnailImages.classList.add("thumbnail_images");
+                thumbnailImages.src = dataDetail.image_link;
+
+                const thumbnail = document.createElement("div");
+                thumbnail.classList.add("thumbnail");
+
+                const thumbnailProfilePic = document.createElement("img");
+                thumbnailProfilePic.classList.add("thumbnail_profile_pic");
+                thumbnailProfilePic.src = `./image/Avatar/${avatarName}`
+
+                const thumbnailDesc = document.createElement("div");
+                thumbnailDesc.classList.add("thumbnail_desc");
+
+                const thumbnailDescTitle = document.createElement("div");
+                thumbnailDescTitle.classList.add("thumbnail_desc_title");
+                thumbnailDescTitle.textContent = dataDetail.video_title;
+
+                const thumbnailDescInfo = document.createElement("div");
+                thumbnailDescInfo.classList.add("thumbnail_desc_info");
+                thumbnailDescInfo.textContent = dataDetail.video_channel;
+                thumbnailDescInfo.textContent = dataDetail.views;
+                thumbnailDescInfo.textContent = dataDetail.upload_date;
+
+
+                thumbnailDesc.appendChild(thumbnailDescTitle);
+                thumbnailDesc.appendChild(thumbnailDescInfo);
+
+                thumbnail.appendChild(thumbnailProfilePic);
+                thumbnail.appendChild(thumbnailDesc);
+
+                thumbnailItem.appendChild(thumbnailImages);
+                thumbnailItem.appendChild(thumbnail);
+
+                videoList.appendChild(thumbnailItem);
+
+            })
+            .catch((error) => {
+
+                console.error(error);
+            });
+    }
+}
